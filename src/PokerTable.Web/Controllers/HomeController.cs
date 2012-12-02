92750898669927cddc204254991e9cf45bda7fc2
Dave <info@devdave.com>
2012-12-01 19:08:41 -0800

    Table storage works, removing create table on index view

--- src/PokerTable.Web/Controllers/HomeController.cs
+++ src/PokerTable.Web/Controllers/HomeController.cs
@@ -18,12 +18,6 @@ namespace PokerTable.Web.Controllers
         /// <returns>the index view</returns>
         public ActionResult Index()
         {
-            // lets create a table and have it saved to table storage so 
-            // we can see if the app setting in azure is working correctly.
-            var engine = new Engine();
-            engine.CreateNewTable(5, "Test", "TestPassword");
-            ViewBag.TableId = engine.Table.Id.ToString();
-
             return this.View();
         }
     }
